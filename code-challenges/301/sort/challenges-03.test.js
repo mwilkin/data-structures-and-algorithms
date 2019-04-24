3d9e7fb56377fa68b24f06567d46b4e5503e097b
Matt Wilkin <mwilkin22@gmail.com>
2019-04-24 16:32:14 -0700

    challenge 3 passes.

--- code-challenges/301/sort/challenges-03.test.js
+++ code-challenges/301/sort/challenges-03.test.js
@@ -43,7 +43,10 @@ Write a function named sortByLength that takes in an array of strings and return
 ------------------------------------------------------------------------------------------------ */
 
 const sortByLength = (arr) => {
-  // Solution code here...
+  arr.sort ( (a, b) => {
+    return a.length - b.length;
+  });
+  return arr;
 };
 
 /* ------------------------------------------------------------------------------------------------
