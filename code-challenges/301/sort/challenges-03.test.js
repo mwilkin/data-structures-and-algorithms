d38cbda9cc5f25aa39937d39873bdefb8d36e62d
Matt Wilkin <mwilkin22@gmail.com>
2019-04-24 17:00:12 -0700

    challenge 7 passes.

--- code-challenges/301/sort/challenges-03.test.js
+++ code-challenges/301/sort/challenges-03.test.js
@@ -126,7 +126,16 @@ const people = [
 ];
 
 const sortPeople = (arr) => {
-  // Solution code here...
+  arr.sort( (a, b) => {
+    if(a.lastName < b.lastName){
+      return -1;
+    }
+    if(a.lastName > b.lastName){
+      return 1;
+    }
+    return 0;
+  });
+  return arr;
 };
 
 /* ------------------------------------------------------------------------------------------------
