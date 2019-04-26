77bb0fbd61cbda9b928778f39f3f09889137d32a
Matt Wilkin <mwilkin22@gmail.com>
2019-04-26 09:03:00 -0700

    challenges 1-6 passing, challenge 7 attempted but not passing.

--- code-challenges/301/regex-pt1/challenges-04.test.js
+++ code-challenges/301/regex-pt1/challenges-04.test.js
@@ -61,7 +61,7 @@ Do not use the vertical bar (pipe) in your pattern.
 ------------------------------------------------------------------------------------------------ */
 
 const matchMonth = (input) => {
-  let halloween = /[oct]{3}?(ober)?(\b)*/gi;
+  let halloween = /\b([Oo]ct)(\b)?(ober)?(\b)/g;
   return halloween.test(input);
 };
 // regexr.com indicates that this should work!?
@@ -112,13 +112,14 @@ Hint: All of these words end with the letters "ells".
 const seashells = 'She sells seashells by the seashore. The shells she sells are surely seashells. So if she sells shells on the seashore, I\'m sure she sells seashore shells.';
 
 const findShells = (str) => {
-  let magicStuff = /ells$/g;
-  let holdingArr = [];
-  if(str.match(magicStuff).holdingArr.push()
+  // let magicStuff = /ells$/g;
+  // let holdingArr = [];
+  // if(str.match(magicStuff).holdingArr.push()
+  // )
 };
 
 /* ------------------------------------------------------------------------------------------------
-TESTS
+TEST
 
 All the code below will verify that your functions are working to solve the challenges.
 
