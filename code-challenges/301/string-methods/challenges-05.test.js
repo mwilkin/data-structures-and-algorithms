0874c5b177f0490929309e658ee3057a28f27314
Matt Wilkin <mwilkin22@gmail.com>
2019-04-28 20:45:27 -0700

    challenge 2 passes.

--- code-challenges/301/string-methods/challenges-05.test.js
+++ code-challenges/301/string-methods/challenges-05.test.js
@@ -30,7 +30,7 @@ For example, wordsToCharList('gregor') returns ['g','r','e','g','o','r'].
 ------------------------------------------------------------------------------------------------ */
 
 const wordsToCharList = (arr) => {
-  // Solution code here...
+  return arr.split('');
 };
 
 
