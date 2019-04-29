507a5a913f713cfab7418d4e881c8d8a7b3f89fa
Matt Wilkin <mwilkin22@gmail.com>
2019-04-29 16:41:25 -0700

    challenge 2 passes.

--- code-challenges/301/object-iteration/challenges-06.test.js
+++ code-challenges/301/object-iteration/challenges-06.test.js
@@ -70,7 +70,9 @@ let characters = [
 
 const getHouses = (arr) => {
   let houses = [];
-  // Solution code here...
+  arr.forEach(element => {
+    houses.push(element.house);
+  });
   return houses;
 };
 
