466ba9413235ac2e34bf4b39b8ce02ceb55f7db2
Matt Wilkin <mwilkin22@gmail.com>
2019-05-20 20:19:56 -0700

    add example input/output to README and function reverseArray.

---
+++ code-challenges/401/arrayReverse/array-reverse.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = exports = {};
+
+exports.reverseArray = (inputArray) => {
+  let reversedArr = [];
+  let counter = 0;
+
+  for (let i = inputArray.length -1; i>= 0; i--){
+    reversedArr[counter] = inputArray[i];
+    counter++;
+  }
+  return reversedArr;
+}
