a463b5d07e1c6c52a10af8e1c2072df425fb52d1
Matt Wilkin <mwilkin22@gmail.com>
2019-05-21 19:09:43 -0700

    add test directory and unit tests, establish functionality of insertShiftArray functionality.

--- code-challenges/401/arrayShift/array-shift.js
+++ code-challenges/401/arrayShift/array-shift.js
@@ -1,1 +1,15 @@
 'use strict';
+
+module.exports = exports {};
+
+exports.insertShiftArray = (array, item) => {
+  if(typof array !== 'array'){return null; }
+  let middleIndex = Math.ceil(array.length/2);
+  
+  for( let i = array.length -1; i >= middleIndex ; i-- ){
+    array[i + 1] = array[i];
+  }
+  array[middleIndex] = item;
+
+  return array;
+}
