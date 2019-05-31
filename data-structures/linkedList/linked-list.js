08cf52af4c82da233ac46269a8c48bd604762843
Matt Wilkin <mwilkin22@gmail.com>
2019-05-31 14:04:54 -0700

    refactor into class, insert not implemented yet.

--- data-structures/linkedList/linked-list.js
+++ data-structures/linkedList/linked-list.js
@@ -1,47 +1,51 @@
 'use strict';
 
 class LinkedListNode {
-  constructor(data) {
-    this.data = data;
-    // this.next = null;
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    // this.head = null;
+  }
+}
+
+class LinkedList{
+  constructor() {
     this.head = null;
   }
 
-  insert(data){
-    const newNode = new LinkedListNode(data);
-
-    this[head] = newNode;
-
-    // if(this[head] === null) {
-    //   this[head] = newNode;
-    // } else {
-    //   let current = this[head];
-    //   while(current.next !== null){
-    //     current = current.next;
-    //   }
-    //   current.next = newNode;
-    // }
+  insert(value){
+    let node = new LinkedListNode(value);
+    node.value = value;
+    node.next = this.head;
+    this.head = node;
+    return node.value;
   }
 
-  includes(data){
+  includes(value){
     //returns a boolean result depending on whether that value exists as a Node’s value somewhere within the list.
   }
 
   printList(){
     let current = this.head;
-    let string = '';
+    let results = [];
     while(current){
-      str += current.element + ' ';
+      results.push(current.value);
       current = current.next;
     }
-    console.log(string);
-    //returns a collection all of the current Node values in the Linked List.
+    return results;
+
+    // let string = '';
+    // while(current){
+    //   str += current.element + ' ';
+    //   current = current.next;
+    // }
+    // console.log(string);
+    // //returns a collection all of the current Node values in the Linked List.
+    // return string;
   }
 }
 
+
 // Catch and handle any such exceptions and return a printed value or operation which cleanly represents the state and either stops execution cleanly, or provides the user with clear direction and output.
 
-let ed = new LinkedListNode();
-
-console.log(ed);
-// console.log(ed.printList());
+module.exports = LinkedList;
