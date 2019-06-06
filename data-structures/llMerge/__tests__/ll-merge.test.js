388a4eff86ac29073e8d4178f98af159485403b2
Matt Wilkin <mwilkin22@gmail.com>
2019-06-06 10:58:21 -0700

    correct naming error in test file, all five tests are passing.

--- data-structures/llMerge/__tests__/ll-merge.test.js
+++ data-structures/llMerge/__tests__/ll-merge.test.js
@@ -36,13 +36,13 @@ describe('Merged Linked List Module', () => {
   })
 
   it('should contain a value from list 2 after successful merge', ()=> {
-    let node3 = {val: 3, next: null};
-    let node2 = {val: 2, next: node3};
-    let node1 = {val: 1, next: node2};
+    let node3 = {value: 3, next: null};
+    let node2 = {value: 2, next: node3};
+    let node1 = {value: 1, next: node2};
 
-    let node13 = {val: 30, next: null};
-    let node12 = {val: 20, next: node13};
-    let node10 = {val: 10, next: node12};
+    let node13 = {value: 30, next: null};
+    let node12 = {value: 20, next: node13};
+    let node10 = {value: 10, next: node12};
 
     list1.head = node1;
     list1.length = 3;
@@ -50,7 +50,9 @@ describe('Merged Linked List Module', () => {
     list2.head = node10;
     list2.length = 3;
     let newList = mergedLinkedLists(list1, list2);
+    expect(newList.value).toEqual(1);
     expect(newList.next.next.next.value).toEqual(20);
+    expect(newList.next.next.next.next.next.value).toEqual(30);
   });
 
   it('can merge two lists of the same length', () => {
@@ -61,7 +63,7 @@ describe('Merged Linked List Module', () => {
     
     list1.head = node;
     list1.length = 4;
-    console.log(list1.head);
+  
     list2.head = node;
     list2.length = 4;
 
