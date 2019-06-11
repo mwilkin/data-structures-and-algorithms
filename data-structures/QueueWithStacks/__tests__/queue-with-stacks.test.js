6068a1fbfdd2ddbbfd2e768ee9567a2ebb52bc02
Matt Wilkin <mwilkin22@gmail.com>
2019-06-10 21:04:34 -0700

    first test passing, instantiation of a queue.

--- data-structures/QueueWithStacks/__tests__/queue-with-stacks.test.js
+++ data-structures/QueueWithStacks/__tests__/queue-with-stacks.test.js
@@ -1,4 +1,20 @@
 'use strict'
 
-const queuesAndStacks = require('../stacks-and-queues');
+const QueuesAndStacks = require('../queue-with-stacks');
 
+// const QueuesAndStacks = require('../../stacksAndQueues/stacks-and-queues.js');
+
+describe('Queue with Stacks Tests', () => {
+  let pseudoQueue;
+
+  beforeEach(() => {
+    pseudoQueue = new QueuesAndStacks();
+  });
+
+  it('should instantiate a queue', () => {
+    expect(pseudoQueue).toBeDefined();
+    expect(pseudoQueue).toBeInstanceOf(QueuesAndStacks);
+  });
+
+
+});
--- data-structures/QueueWithStacks/queue-with-stacks.js
+++ data-structures/QueueWithStacks/queue-with-stacks.js
@@ -31,7 +31,7 @@ class Stack {
   }
 
   peek() {
-    it(!this.top){
+    if(!this.top){
       return null;
     } else {
       return this.top.value;
@@ -39,26 +39,28 @@ class Stack {
   }
 }
 
-class PseudoQueue(){
+class PseudoQueue{
   constructor(){
     this.stack1 = new Stack();
     this.stack2 = new Stack();
   }
-}
 
-enqueue(value){
-  this.stack1.push(value);
-}
-
-dequeue(){
-  if(this.stack2.length === 0){
-    if(this.stack1.length === 0) return 'Queue is empty';
-    while(this.stack1.length > 0){
-      let temp = this.stack1.pop();
-      this.stack2.push(temp);
-    }
+  enqueue(value){
+    this.stack1.push(value);
   }
-  return this.stack2.pop();
+  
+  dequeue(){
+    if(this.stack2.length === 0){
+      if(this.stack1.length === 0) return 'Queue is empty';
+      while(this.stack1.length > 0){
+        let temp = this.stack1.pop();
+        this.stack2.push(temp);
+      }
+    }
+    return this.stack2.pop();
+  }
 }
+
+
 
 module.exports = PseudoQueue;
