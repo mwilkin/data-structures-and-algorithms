b6ac69e6632b7499e6389baf5e29d907ae20d6c7
Matt Wilkin <mwilkin22@gmail.com>
2019-06-12 14:13:03 -0700

    establish project files, update README.

--- data-structures/multiBracketValidation/__tests__/mulit-bracket-validation.tests.js
+++ data-structures/multiBracketValidation/__tests__/mulit-bracket-validation.tests.js
@@ -1,1 +1,13 @@
 'use strict';
+
+// test for a string
+// test for an empty string returns not valid
+// test for one element of a string returns not valid
+// test for two matching brackets return true
+// test for more than two matching brackets return true
+// test for two unbalenced brackets returning false
+// test for multiple unbalence brackets return false
+// test for balenced brakets with other characters returns true
+// test for unbalenced with other characters returns false
+
+const multiBracketValidation = require('./multi-bracket-validation')
--- data-structures/multiBracketValidation/multi-bracket-validation.js
+++ data-structures/multiBracketValidation/multi-bracket-validation.js
@@ -1,1 +1,4 @@
 'use strict';
+const stacks = require('../stacksAndQueues/stacks-and-queues.js');
+
+ module.exports = multiBranchValidation
