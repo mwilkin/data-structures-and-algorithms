baf97896ee1ebb36d6bcd1f8d8a61b6740f84e7d
Matt Wilkin <mwilkin22@gmail.com>
2019-06-14 16:43:16 -0700

    inOrder function

--- data-structures/tree/tree.js
+++ data-structures/tree/tree.js
@@ -7,6 +7,35 @@ class BinarySearchTree {
     this.root = node;
   }
 
+  preOrder(){
+
+  }
+
+  inOrder(){
+    let results = [];
+
+    let _walk = node => {
+      //Left Route Right
+
+
+      //Left
+      if(node.lef) _wak(node.left);
+
+      //Route
+      results.push(node.value);
+
+      //Right
+      if(node.right) _walk(node.right);
+    };
+
+    _walk(this.root);
+    return results;
+  }
+
+  postOrder(){
+
+  }
+
   add(node) {
     if(!this.root){
       this.root = node;
@@ -15,14 +44,29 @@ class BinarySearchTree {
 
     let currentNode = this.root;
 
-    if(node.value < currentNode.value){
-      currentNode.left = node;
-    } else if (node.value > currentNode.value){
-      currentNode.right = node;
-    }else {
-      //node values are equal
-    }
+    while(currentNode){
+      if(node.value < currentNode.value){
+        //Go left
+        if(!currentNode.left){
+          currentNode.left = node;
+          break;
+        }else{
+          currentNode = currentNode.left;
+        }
+      } else if (node.value > currentNode.value){
+        //Go right
+        if(!currentNode.right){
+          currentNode.right = node;
+          break;
+        }else{
+          currentNode = currentNode.right;
+        }
+      }else {
+        //duplicate value found
+        throw new Error('Value already exists in Binary Search Tree');
+      }
   }
+
 
 }
 
