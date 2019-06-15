6a65d5330fe00c83827b286cde3716c23bd6a8fb
Matt Wilkin <mwilkin22@gmail.com>
2019-06-14 21:22:53 -0700

    add tests.

--- data-structures/tree/__tests__/tree.test.js
+++ data-structures/tree/__tests__/tree.test.js
@@ -6,3 +6,74 @@
 // Can successfully return a collection from a preorder traversal
 // Can successfully return a collection from an inorder traversal
 // Can successfully return a collection from a postorder traversal
+
+
+'use strict';
+
+const Tree = require('../tree.js');
+const TreeNode = require('../node/index.js');
+
+describe('tree constructor', () => {
+
+  it('tree constructor returns object', () => {
+    let actual = new Tree();
+
+    expect(typeof actual).toBe('object');
+  })
+
+  it('tree constructor returns object with root and value', () => {
+    let node = new TreeNode(5);
+    let actual = new Tree(node);
+
+    expect(actual.root.value).toBe(5);
+  })
+
+});
+
+
+describe('tree add method', () => {
+
+  it('Adding a node adds to root', () => {
+    let testTree = new Tree(new TreeNode(5));
+    expect(testTree.root.value).toBe(5);
+  });
+
+  it('Adding 4 nodes adds to root children properly', () => {
+    let testTree = new Tree(new TreeNode(3));
+    testTree.add(new TreeNode(5));
+    testTree.add(new TreeNode(4));
+    testTree.add(new TreeNode(7));
+    expect(testTree.root.right.left.value).toBe(4);
+    expect(testTree.root.right.right.value).toBe(7);
+  });
+
+  it('add method only works with numbers', () => {
+    expect(() => {
+      let testTree = new Tree(new TreeNode(5));
+      testTree.add(new TreeNode('asdf'));
+      console.log(testTree);
+    }).toThrow('add method only works with numbers');
+  });
+
+});
+
+describe('contains method', () => {
+
+  let testTree = new Tree(new TreeNode(5));
+  let node3 = new TreeNode(3);
+  let node7 = new TreeNode(7);
+
+  it('that contains method returns root node from BST with 1 node', () => {
+    let actual = testTree.find(5);
+    expect(actual.value).toBe(5);
+  })
+
+  it('that contains method returns a node from BST with 3 nodes', () => {
+    testTree.root.right = node7;
+    testTree.root.left = node3;
+    let actual = testTree.find(3);
+    expect(actual.value).toBe(3);
+    expect(actual.right).toBe(null);
+
+  })
+});
