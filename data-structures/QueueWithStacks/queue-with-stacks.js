dd722951d840abe334c820de063e6185bc086eb8
Matt Wilkin <mwilkin22@gmail.com>
2019-06-19 13:20:28 -0700

    fixing queue with stack

--- data-structures/QueueWithStacks/queue-with-stacks.js
+++ data-structures/QueueWithStacks/queue-with-stacks.js
@@ -48,7 +48,7 @@ class PseudoQueue{
   enqueue(value){
     this.stack1.push(value);
   }
-  
+
   dequeue(){
     if(this.stack2.length === 0){
       if(this.stack1.length === 0) return 'Queue is empty';
