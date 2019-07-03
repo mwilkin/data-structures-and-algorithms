8b743acd31612fb7e4aae4cb619196ae914dba8c
Matt Wilkin <mwilkin22@gmail.com>
2019-07-02 21:40:46 -0700

    add tests, passing.

--- data-structures/sorts/merge/__tests__/mergeSort.test.js
+++ data-structures/sorts/merge/__tests__/mergeSort.test.js
@@ -1,7 +1,5 @@
 'use strict';
 
-'use strict';
-
 const mergeSort = require('../mergeSort.js');
 
 describe('mergeSort', () => {
@@ -9,16 +7,32 @@ describe('mergeSort', () => {
     const arr = [1, 1, 2, 3, 4, 5, 99, 1000, 10021];
     expect(mergeSort(arr)).toEqual(arr);
   });
+
+  it('Should sort an array with an even amount of values', () => {
+    let arr = [9, 5, 6, 2, 5, 1];
+    let sorted = mergeSort(arr);
+    expect(sorted).toEqual([1, 2, 5, 5, 6, 9]);
+  });
+
+  it('Should sort an array with an odd amount of values', () => {
+    let arr = [9, 70, 5, 3, 1, 21, 12];
+    let sorted = mergeSort(arr);
+    expect(sorted).toEqual([1, 3, 5, 9, 12, 21, 70]);
+  });
+
   it('should return a backwards-sorted array in sorted order', () => {
     const backwards = [12121, 1221, 99, 88, 77, 66, 44, 22, 1, 0.123];
     expect(mergeSort(backwards)).toEqual(backwards.reverse());
   });
+
   it('should return an empty array argument unchanged', () => {
     const empty = [];
     expect(mergeSort(empty)).toEqual(empty);
   });
+
   it('should return a single-element array unchanged', () => {
     const one = [1];
     expect(mergeSort(one)).toEqual(one);
   });
+
 });
