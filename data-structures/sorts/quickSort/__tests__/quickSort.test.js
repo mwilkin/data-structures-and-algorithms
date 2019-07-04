4b3d39cd5279e2bf0a53796535b47c844f0c529b
Matt Wilkin <mwilkin22@gmail.com>
2019-07-03 20:46:01 -0700

    code working for 3 tests.

--- data-structures/sorts/quickSort/__tests__/quickSort.test.js
+++ data-structures/sorts/quickSort/__tests__/quickSort.test.js
@@ -4,21 +4,20 @@ const quickSort = require('../quickSort.js');
 
 describe('quickSort', () => {
   let testArray = [97, 1, 3, 2, 48, 96];
-  let testArray2 = [-5, 18, 9.5, 0]
+  let testArray2 = [-5, 18, 9.5, 0];
   it('should show that values are sorted', () => {
 
-    let actual = quickSort(testArray)
-    expect(actual).toEqual([1, 2, 3, 48, 96, 97])
+    let actual = quickSort(testArray);
+    expect(actual).toEqual([1, 2, 3, 48, 96, 97]);
   });
 
   it('should show that negative values are sorted', () => {
 
-    let actual = quickSort(testArray2)
-    expect(actual).toEqual([-5, 0, 9.5, 18])
+    let actual = quickSort(testArray2);
+    expect(actual).toEqual([-5, 0, 9.5, 18]);
   });
 
   it('shows empty array sorts to empty array', () => {
-  
     let actual = quickSort([]);
     expect(actual).toEqual([]);
   });
@@ -28,7 +27,7 @@ describe('quickSort', () => {
     const array = [1, 1, 2, 3, 4, 5, 99, 1000, 10021];
     expect(quickSort(arr)).toEqual(array);
   });
-  
+
   it('should return a backwards-sorted array in sorted order', () => {
     const backwards = [12121, 1221, 99, 88, 77, 66, 44, 22, 1, 0.123];
     const forwards = [0.123, 1, 22, 44, 66, 77, 88, 99, 1221, 12121];
@@ -38,6 +37,26 @@ describe('quickSort', () => {
   it('should return a single-element array unchanged', () => {
     const one = [1];
     expect(quickSort(one)).toEqual(one);
+  });
+
+  it('should still sort an array of only length 2', () => {
+    let testShort = [22, 7];
+    quickSort(testShort, 0, 1);
+    expect(testShort).toEqual([7, 22]);
+  });
+
+  it('it should only take in an array', () => {
+    let badArray = 'thisverybad';
+    expect(() => {
+      quickSort(badArray, 0, 1);
+    }).toThrow('Input not a valid array');
+  });
+
+  it('it should only take an array of all numbers', () => {
+    let badStringArray = [1, 3, 4, 'thisisverybad', 6, 2];
+    expect(() => {
+      quickSort(badStringArray, 0, badStringArray.length - 1);
+    }).toThrow('Invalid data in array');
   });
 
 });
--- data-structures/sorts/quickSort/quickSort.js
+++ data-structures/sorts/quickSort/quickSort.js
@@ -1,2 +1,50 @@
 'use strict';
 
+'use strict';
+
+function quickSort(arr, left, right){
+  if(!Array.isArray(arr)){
+    throw 'Input not a valid array';
+  }
+
+  if(left < right){
+
+    if(typeof arr[left] !== 'number' || typeof arr[right] !== 'number'){
+      throw 'Invalid data in array';
+    }
+    // Partition the array by setting the position of the pivot value
+    let position = partition(arr, left, right);
+    // Sort the left
+    quickSort(arr, left, position - 1);
+    // Sort the right
+    quickSort(arr, position + 1, right);
+  }
+}
+
+function partition(arr, left, right){
+  // set a pivot value as a point of reference
+  let pivot = arr[right];
+  // create a variable to track the largest index of numbers lower than the defined pivot
+  let low = left - 1;
+  for( let i = left; i <= right - 1; i++){
+
+    if(arr[i] <= pivot){
+      low++;
+      swap(arr, i, low);
+    }
+  }
+  // place the value of the pivot location in the middle.
+  // all numbers smaller than the pivot are on the left, larger on the right.
+  swap(arr, right, low + 1);
+  // return the pivot index point
+  return low + 1;
+}
+
+function swap(arr, leftIndex, low){
+
+  let temp = arr[leftIndex];
+  arr[leftIndex] = arr[low];
+  arr[low] = temp;
+}
+
+module.exports = quickSort;
