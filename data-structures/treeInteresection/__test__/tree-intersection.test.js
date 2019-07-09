724cc8982892c14bf4ac1438b445963898389d46
Matt Wilkin <mwilkin22@gmail.com>
2019-07-09 14:04:04 -0700

    test passing for bad tree input.

--- data-structures/treeInteresection/__test__/tree-intersection.test.js
+++ data-structures/treeInteresection/__test__/tree-intersection.test.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const Tree = require('../../tree/tree.js');
+const treeIntersection = require('../tree-intersection.js');
+
+
+describe('treeIntersection function', () => {
+  it('should show throw and error if two trees are given as parameters', () => {
+    let badTree='';
+    let badTree2 = 'badtree';
+    expect(() => {
+      treeIntersection(badTree, badTree2);
+    }).toThrow('Invalid input: treeIntersection() requires two trees');
+  });
+
+  // it('shoud not return duplicate values', () => {
+
+  // });
+
+  // it('should return an array of matching values', ()=>{
+
+  // });
+
+
+});
