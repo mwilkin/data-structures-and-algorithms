bfaf4352015073ba998d7e4ca215c56f201f0d62
Matt Wilkin <mwilkin22@gmail.com>
2019-07-10 22:27:14 -0700

    add first test.

--- data-structures/leftJoin/__tests__/left-join.test.js
+++ data-structures/leftJoin/__tests__/left-join.test.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const leftJoin = require('../left-join.js');
+
+describe('leftJoin function on hashtables', () => {
+  
+
+  it('should not return the keys of antonyms', () =>{
+    let test = leftJoin(synonymHash, antonymHash);
+    expect(test.flow).toBeFalse();
+  });
+});
--- data-structures/leftJoin/left-join.js
+++ data-structures/leftJoin/left-join.js
@@ -1,1 +1,13 @@
 'use strict';
+const leftJoin = ( leftHash, rightHash) => {
+  if(!leftHash || !rightHash)throw new Error('Not valid input. Two hashtables required');
+  let results = leftHash;
+  for (const key in rightHash){
+    if (leftHash[key]){
+      results[key] = [leftHash[key], rightHash[key]];
+    }
+  }
+  return results;
+};
+
+module.exports = leftJoin;
