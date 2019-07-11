88e4d6daf52ee52b3bb93a50ca227669d0e8e4f3
Matt Wilkin <mwilkin22@gmail.com>
2019-07-10 22:33:25 -0700

    1test passing, antonym key false.

--- data-structures/leftJoin/__tests__/left-join.test.js
+++ data-structures/leftJoin/__tests__/left-join.test.js
@@ -3,10 +3,22 @@
 const leftJoin = require('../left-join.js');
 
 describe('leftJoin function on hashtables', () => {
-  
+  let synonymHash = {};
+  synonymHash.fond = 'enamored';
+  synonymHash.wrath = 'anger';
+  synonymHash.diligent = 'employed';
+  synonymHash.outfit = 'garb';
+  synonymHash.guide = 'usher';
+
+  let antonymHash = {};
+  antonymHash.fond = 'averse';
+  antonymHash.wrath = 'delight';
+  antonymHash.diligent = 'idle';
+  antonymHash.guide = 'follow';
+  antonymHash.flow = 'jam';
 
   it('should not return the keys of antonyms', () =>{
     let test = leftJoin(synonymHash, antonymHash);
-    expect(test.flow).toBeFalse();
+    expect(test.flow).toBeFalsy();
   });
 });
