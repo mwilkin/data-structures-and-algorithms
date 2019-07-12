481769740761e73c97d30a5b780a3f00a1504c89
Matt Wilkin <mwilkin22@gmail.com>
2019-07-12 14:55:28 -0700

    update readme, add suggested tests, continue deving.

---
+++ data-structures/graph/README.md
@@ -0,0 +1,29 @@
+# Graph Implementation
+
+Implement a Graph
+
+## Challenge
+
+* 
+
+## Example
+
+<img src="./assets/left_join_example.png" width=600>
+
+## Approach & Efficiency
+
+I reviewed the class demo, did a lot of research to learn more about Stacks and Queues, and then planned my project, wrote my tests, then executed the code.
+
+All tests are passing, all the code is covered by tests.
+
+## Solution
+
+<img src="./assets/left_join_whiteboard.jpg" width="400">
+
+## Structure and Testing
+
+Utilize the Single-responsibility principle: any methods you write should be clean, reusable, abstract component parts to the whole challenge. You will be given feedback and marked down if you attempt to define a large, complex algorithm in one function definition.
+
+Write at least three test assertions for each method that you define.
+
+------
--- data-structures/graph/graph.js
+++ data-structures/graph/graph.js
@@ -27,10 +27,93 @@ class Graph {
   }
 
   addDirectedEdge(startVertex, endVertex, weight = 0){
+    if(!this._adjacencyList.has(startVertex) || !this._adjacencyList.has(endVertex)){
+      throw new Error('Error: Invalid Vertices');
+    }
 
+    const adjacencies = this._adjacencyList.get(startVertex);
+    adjacencies.push(new Edge(endVertex, weight));
+  }
+
+  addBiDirectionalEdge(vertex_a, vertex_b, weight =0){
+    this.addDirectedEdge(vertex_a, vertex_b, weight);
+    this.addDirectedEdge(vertex_a, vertex_b, weight);
+  }
+
+  getNeighbors(vertex){
+    if(!this._adjacencyList.has(vertex)){
+      throw new Error('Error: Invalid vertex', vertex);
+    }
+
+    return [...this._adjacencyList.get(vertex)];
+  }
+
+  pathTo(startVertex, goalVertex){
+    const stack = [];
+    const visitedVertices = new Set();
+    const parentPath = new Map();
+    stack.push(startVertex);
+    visitedVertices.add(startVertex);
+
+    while(stack.length){
+      const currentVertex = stack.pop();
+
+      console.log(currentVertex, goalVertex);
+      if(currentVertex === goalVertex){
+        return parentPath;
+      }
+
+      const neighbors = this.getNeighbors(currentVertex);
+
+      for(let edge in neighbors){
+        const neighborVertex = edge.vertex;
+
+        if(visitedVertices.has(neighborVertex)){
+          continue;
+        } else {
+          visitedVertices.add(neighborVertex);
+        }
+        stack.push(neighborVertex, currentVertex);
+      }
+    }
+  }
+
+  prettPrintAdjacencyList(){
+    //Iterate over all kes in map
+    //For each key, print to scree
+    //Print vertex in all edges
   }
 
 }
 
-module.exports = Graph;
+const graph = new Graph();
 
+const eight = new Vertex(8);
+const six = new Vertex(6);
+const seven = new Vertex(7);
+const five = new Vertex(5);
+const three = new Vertex(3);
+const oh = new Vertex(0);
+const nine = new Vertex(9);
+
+graph.addVertex(eight);
+graph.addVertex(six);
+graph.addVertex(seven);
+graph.addVertex(five);
+graph.addVertex(three);
+graph.addVertex(oh);
+graph.addVertex(nine);
+
+graph.addDirectedEdge(eight, six);
+graph.addDirectedEdge(eight, five);
+graph.addDirectedEdge(six, seven);
+graph.addDirectedEdge(seven, five);
+graph.addDirectedEdge(five, three);
+graph.addDirectedEdge(three, oh);
+graph.addDirectedEdge(oh, nine);
+graph.addDirectedEdge(nine, eight);
+
+console.log(graph.getNeighbors(eight));
+
+
+module.exports = Graph;
