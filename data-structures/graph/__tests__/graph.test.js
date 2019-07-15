1bf04026f2d053dfbf909627e1039546e3594c12
Matt Wilkin <mwilkin22@gmail.com>
2019-07-14 19:10:28 -0700

    size method passing.

--- data-structures/graph/__tests__/graph.test.js
+++ data-structures/graph/__tests__/graph.test.js
@@ -4,13 +4,15 @@ const { Graph, Node, Vertex, Edge } = require('../graph.js');
 
 describe('Graph data structure', ()=> {
   let graph;
+  let five;
+  let six;
   beforeEach(() => {
     graph = new Graph();
 
     const eight = new Vertex(8);
-    const six = new Vertex(6);
+    six = new Vertex(6);
     const seven = new Vertex(7);
-    const five = new Vertex(5);
+    five = new Vertex(5);
     const three = new Vertex(3);
     const oh = new Vertex(0);
     const nine = new Vertex(9);
@@ -46,7 +48,11 @@ describe('Graph data structure', ()=> {
     });
 
   });
-  // it('should successfully add an edge to the graph', () => {
+  // describe('addDirectedEdge method', () => {
+  //   it('should successfully add an edge to the graph', () => {
+  //     graph.addDirectedEdge(five, six, 2);
+  //     expect(graph[five].toHave)
+  //   });
 
   // });
   // it('should properly retrieve a collection of all nodes from the graph', () => {
@@ -58,9 +64,9 @@ describe('Graph data structure', ()=> {
   // it('should return neighbors with the weight between nodes included', () => {
 
   // });
-  // it('should return the proper size, representing the number of nodes in the graph', () => {
-
-  // });
+  it('should return the proper size, representing the number of nodes in the graph', () => {
+    expect(graph.size()).toEqual(7);
+  });
   // it('should properly return a graph with only one node and edge', () => {
 
   // });
