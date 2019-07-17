fbaa3ab48394da6a2a3d5002dbfd0abf172dcdf3
Matt Wilkin <mwilkin22@gmail.com>
2019-07-16 23:01:36 -0700

    one node graph test passing.

--- code-challenges/401/getEdge/__tests__/get-edge.test.js
+++ code-challenges/401/getEdge/__tests__/get-edge.test.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const getEdges = require('../get-edge.js');
+const { Graph } = require('../../../../data-structures/graph/graph'); 
+
+describe('getEdges method', () => {
+  it('should return [false, 0] for graph with one node', () => {
+    const graph = new Graph();
+
+    const a = graph.addNode('a');
+
+    let result = getEdges(graph, [a]);
+
+    expect(result).toEqual([false, 0]);
+  });
+
+
+});
+
